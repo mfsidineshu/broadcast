78ab162a80ca5687d8d00c68705c7c507d49fcc2
mfsidineshu <mindIsOnFire@@1>
2021-06-21 15:28:44 +0530

    basic erxamples

---
+++ app/GraphQL/Queries/Greet.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+class Greet
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($rootValue, array $args): string
+    {
+        // TODO implement the resolver
+        return "Hello, {$args['name']}!";
+    }
+}
